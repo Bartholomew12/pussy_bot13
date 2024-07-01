a939ce793ce7b8e51c142162601e169876b01393
Bartholomew12 <bartholomewfolarin7@gmail.com>
2024-07-01 01:07:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,31 +16,31 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V13' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/N3kyujoG3vk"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.botname = '𝗹𝗼𝗻𝗲𝗿.𝘅𝗻🫠' //ur bot name
+global.ownernumber = '2348184186086' //ur owner number
+global.ownername = '𝗹𝗼𝗻𝗲𝗿.𝘅𝗻🫠' //ur owner name
+global.websitex = "https://chat.whatsapp.com/BTTuDZX787lLk6JmIEE71a"
+global.wagc = "https://chat.whatsapp.com/BTTuDZX787lLk6JmIEE71a"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD13' //script link
+global.wm = "𝗹𝗼𝗻𝗲𝗿 Bot Inc."
+global.botscript = 'https://github.com/dey play' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.author = "𝗹𝗼𝗻𝗲𝗿.𝘅𝗻🫠"
+global.creator = "2348184186086@s.whatsapp.net"
+global.xprefix = ','
+global.premium = ["2348184186086"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v12' // menu type 'v1' => 'v12'
+global.typemenu = 'v8' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.autoblocknumber = '212' //set autoblock country code
+global.antiforeignnumber = '212' //set anti foreign number country code
+global.welcome = true //welcome/left in groups
+global.anticall = true //bot blocks user when called
+global.autoswview = true //auto status/story view
+global.adminevent = true //show promote/demote message
+global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
